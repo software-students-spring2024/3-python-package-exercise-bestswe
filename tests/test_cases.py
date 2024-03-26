fcb69f3d677447c2161986167ff4685f09af8806
Jacklyn22 <jrhu1222@gmail.com>
2024-03-26 16:39:10 -0400

    Updated test cases & Parameters.

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -14,14 +14,6 @@ def test_response_for_valid_input():
         "Yes – in due time.", "Yes, but don't hold your breath.", "You will have to wait.", "I have my doubts.", "The signs suggest no.",
         "The answer lies within yourself.", "Only time will tell.", "The answer is hidden in plain sight."
     ]
-
-    for count in range(1, 6):  # Testing for the first five questions
-        response = fun.magic_8_ball(count)
-        assert response in valid_responses, f"Response '{response}' was not in the list of known responses."
-
-def test_tired_response():
-    # The function returns the "tired" response after 5 questions
-    response = fun.magic_8_ball(6) 
     question = "How's it going?"
     for count in range(1, 5):  # Testing for the first four questions
         response = fun.magic_8_ball(question, count)
