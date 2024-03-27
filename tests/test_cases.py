b5f3af6360d642dbc8307899ddcc617521784cff
Natalie Wu <nw2132@nyu.edu>
2024-03-27 12:50:50 -0400

    fixed invalid choice laugh

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -173,7 +173,9 @@ def test_joke():
     response = fun.make_me_laugh("joke")
     assert response in valid_jokes, f"Response '{response}' was not in the list of valid jokes."
 
-def test_invalid_choice_laugh(self):
-        self.assertEqual(fun.make_me_laugh('duck'), "Please choose either 'fun fact' or 'joke'.")
+def test_invalid_choice_laugh():
+        invalid_response = ["Invalid Type."]
+        response = fun.make_me_laugh("quack")
+        assert response in invalid_response, f"Response '{response}' was not a valid response to an invalid input."
 
     
